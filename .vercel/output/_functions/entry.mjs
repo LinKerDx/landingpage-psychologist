83f04f1e969f4b56cf062a604cb163b597878bcc
LinKDx <omar_150596@hotmail.com>
2025-04-11 17:30:30 -0600

    Casi terminando
    Mandando al repositorio para desplegarlo

---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,34 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_DKBYW7Lv.mjs';
+import { manifest } from './manifest_q4KV2O7i.mjs';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/404.astro.mjs');
+const _page2 = () => import('./pages/about.astro.mjs');
+const _page3 = () => import('./pages/servicespsy/_id_.astro.mjs');
+const _page4 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/.pnpm/astro@5.6.1_typescript@5.8.3/node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/404.astro", _page1],
+    ["src/pages/about.astro", _page2],
+    ["src/pages/ServicesPsy/[id].astro", _page3],
+    ["src/pages/index.astro", _page4]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./_noop-actions.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "16f99428-bc4d-4cf4-b4fd-5356906674c7",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
