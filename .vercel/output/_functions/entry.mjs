814dd9ab0795664312fddbe14610437c83b8528f
LinKDx <omar_150596@hotmail.com>
2025-04-12 11:19:44 -0600

    building again

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_DKBYW7Lv.mjs';
-import { manifest } from './manifest_C3BTc-Ey.mjs';
+import { manifest } from './manifest_CilEOXz0.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -25,7 +25,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "90b8c909-85fc-4276-adc6-403ffb30466f",
+    "middlewareSecret": "1ad6e1fe-c739-4975-b638-19d13ab0a8fd",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
